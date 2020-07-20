7fff45dd203f4b6a8b3866f97e0b70462adaebeb
vipul43 <saifunny43@gmail.com>
2020-07-21 00:11:01 +0530

    setup the initial react app and cleaned up

---
+++ covid19-tracker/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build a covid19 tracker!!</h1>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* Info boxes */}
+      {/* Info boxes */}
+      {/* Info boxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
